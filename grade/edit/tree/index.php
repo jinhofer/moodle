5f20d33e1ed75beb88d35b209b8ac655881222e9
Colin Campbell <colin@umn.edu>
2014-10-30 10:02:36 -0500

    On gradebook "Categories and items" page, copied the action buttons from the bottom of the page to the top.  Used code from part of dev26 commit fd4358297e6662f.

--- grade/edit/tree/index.php
+++ grade/edit/tree/index.php
@@ -246,6 +246,26 @@ if ($data = data_submitted() and confirm_sesskey()) {
 
 print_grade_page_head($courseid, 'settings', 'setup', get_string('categoriesanditems', 'grades'));
 
+// SDLC-84396 20110617 hoang027 >>> repeat the action buttons from bottom of page to the top
+// Print action buttons
+echo $OUTPUT->container_start('buttons mdl-align');
+
+if ($moving) {
+    echo $OUTPUT->single_button(new moodle_url('index.php', array('id'=>$course->id)), get_string('cancel'), 'get');
+} else {
+    echo $OUTPUT->single_button(new moodle_url('category.php', array('courseid'=>$course->id)), get_string('addcategory', 'grades'), 'get');
+    echo $OUTPUT->single_button(new moodle_url('item.php', array('courseid'=>$course->id)), get_string('additem', 'grades'), 'get');
+
+    if (!empty($CFG->enableoutcomes)) {
+        echo $OUTPUT->single_button(new moodle_url('outcomeitem.php', array('courseid'=>$course->id)), get_string('addoutcomeitem', 'grades'), 'get');
+    }
+
+    //echo $OUTPUT->(new moodle_url('index.php', array('id'=>$course->id, 'action'=>'autosort')), get_string('autosort', 'grades'), 'get');
+}
+
+echo $OUTPUT->container_end();
+// <<< SDLC-84396
+
 // Print Table of categories and items
 echo $OUTPUT->box_start('gradetreebox generalbox');
 
