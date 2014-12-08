5df1a2f40acdef98e693c8222a91bd8cb7bb4e59
Clement Smith <cdsmith@oit-moodle-dev-28.oit.umn.edu>
2014-12-08 11:47:42 -0600

    Group assignment submission changes
    STRY0010277
    This is a port of a customization from 2.6 to 2.8 that makes it
    possible for one student to submit a group assignment for grading
    for all students in the group.
    
    The original commit ids were:
    929a7ab00fd4c231dc8
    1ced57adf3b2eb0bc

--- mod/assign/locallib.php
+++ mod/assign/locallib.php
@@ -4301,7 +4301,13 @@ class assign {
         $result = true;
         if ($submission->status != ASSIGN_SUBMISSION_STATUS_REOPENED) {
             foreach ($team as $member) {
-                $membersubmission = $this->get_user_submission($member->id, false, $submission->attemptnumber);
+                //STRY0010277 mart0969 20140414 - Update the submissions for all group members.
+                $membersubmission = $this->get_user_submission($member->id, true, $submission->attemptnumber); //Change now creates a submission for each group member
+
+                //Update the newly-created submission in the database.
+                $membersubmission->status = $submission->status;
+                $this->update_submission($membersubmission, 0, $updatetime, false);
+                //<<< STRY0010277
 
                 // If no submission found for team member and member is active then everyone has not submitted.
                 if (!$membersubmission || $membersubmission->status != ASSIGN_SUBMISSION_STATUS_SUBMITTED
@@ -4952,6 +4958,14 @@ class assign {
             $completion = new completion_info($this->get_course());
             if ($completion->is_enabled($this->get_course_module()) && $instance->completionsubmit) {
                 $completion->update_state($this->get_course_module(), COMPLETION_COMPLETE, $userid);
+
+                //STRY0010277 20140523 mart0969 - Update completion status for group members if this is a group submission
+                if ($instance->teamsubmission) {
+                    $team = $this->get_submission_group_members($submission->groupid, true);
+                    foreach ($team as $member) {
+                        $completion->update_state($this->get_course_module(), COMPLETION_COMPLETE, $member->id);
+                    }
+                }
             }
 
             if (!empty($data->submissionstatement) && $USER->id == $userid) {
@@ -5635,6 +5649,14 @@ class assign {
         $completion = new completion_info($this->get_course());
         if ($completion->is_enabled($this->get_course_module()) && $instance->completionsubmit) {
             $completion->update_state($this->get_course_module(), $complete, $USER->id);
+
+            //STRY0010277 20140523 mart0969 - Update completion status for group members if this is a group submission
+            if ($instance->teamsubmission) {
+                $team = $this->get_submission_group_members($submission->groupid, true);
+                foreach ($team as $member) {
+                    $completion->update_state($this->get_course_module(), $complete, $member->id);
+                }
+            }
         }
 
         if (!$instance->submissiondrafts) {
@@ -5749,6 +5771,14 @@ class assign {
         $completion = new completion_info($this->get_course());
         if ($completion->is_enabled($this->get_course_module()) && $instance->completionsubmit) {
             $completion->update_state($this->get_course_module(), $complete, $userid);
+
+            //STRY0010277 20140523 mart0969 - Update completion status for group members if this is a group submission
+            if ($instance->teamsubmission) {
+                $team = $this->get_submission_group_members($submission->groupid, true);
+                foreach ($team as $member) {
+                    $completion->update_state($this->get_course_module(), $complete, $member->id);
+                }
+            }
         }
 
         if (!$instance->submissiondrafts) {
