39137ebf0ed5a400876e6672e518cdabdc4f56c1
Clement Smith <cdsmith@oit-moodle-dev-28.oit.umn.edu>
2014-12-09 13:43:08 -0600

    Installed local/globalmessage v2.1.4
    This was downloaded from the HEAD of the current master branch of
    https://github.com/satrun77/Global-Message-Moodle-Plugin
    as 82fbfd6457306d6cd3b546edcf91a223932d529c.zip.
    
    The maintainer's specific Github commit id for this version is
    82fbfd6457306d6cd3b546edcf91a223932d529c. This is currently the
    latest stable version compatible with Moodle 2.8.

---
+++ local/globalmessage/version.php
@@ -0,0 +1,6 @@
+<?php
+
+$plugin->version  = 2014110501;   // The (date) version of this plugin
+$plugin->requires = 2014051200;   // Requires this Moodle version
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '2.1.4';
