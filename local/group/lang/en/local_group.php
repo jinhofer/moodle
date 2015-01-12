35763fe5d122f8b80aaa1db5a30b360076045e96
Colin Campbell <colin@umn.edu>
2015-01-12 13:18:46 -0600

    local/group. Added a lang file and version.php.

---
+++ local/group/lang/en/local_group.php
@@ -0,0 +1,4 @@
+<?php
+
+$string['pluginname'] = 'Customizations related to group functionality';
+
---
+++ local/group/version.php
@@ -0,0 +1,6 @@
+<?php
+
+$plugin->version   = 2014092300;   // The (date) version of this plugin
+$plugin->requires  = 2012120300;   // Requires this Moodle version
+$plugin->component = 'local_group';
+
